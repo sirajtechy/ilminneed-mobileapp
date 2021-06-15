2d07a67cbd77a143b884071b78ce9a0b8c5f3b26
sivanesh <sivaneshmca@gmail.com>
2021-06-15 15:46:07 +0530

    Auth

--- lib/src/screen/forgot_password.dart
+++ lib/src/screen/forgot_password.dart
@@ -8,9 +8,50 @@ import 'package:ilminneed/src/ui_helper/text_styles.dart';
 import 'package:ilminneed/src/widgets/button.dart';
 import 'package:ilminneed/src/widgets/header_text.dart';
 import 'package:ilminneed/src/widgets/hint_text.dart';
+import 'package:ilminneed/src/controller/globalctrl.dart' as ctrl;
+import 'package:loading_overlay/loading_overlay.dart';
+import 'package:get/get.dart';
 
-class ForgotPassword extends StatelessWidget {
+class ForgotPassword extends StatefulWidget {
   const ForgotPassword({Key key}) : super(key: key);
+
+  @override
+  _ForgotPasswordState createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+
+  bool _loading = false;
+  final TextEditingController _email = TextEditingController();
+  final GlobalKey<FormState> _formKey  = GlobalKey<FormState>();
+
+  _sendlink() async {
+    if(!_formKey.currentState.validate()) {
+      return;
+    }
+    _formKey.currentState.save();
+    setState(() { _loading = true;});
+    var res = await ctrl.requestwithoutheader({'email': _email.text}, 'forgot/password');
+    setState(() { _loading = false; });
+    if (res != null && res['error'] == null) {
+      await ctrl.toastmsg(res['message'], 'long');
+    } else {
+      setState(() { _loading = false; });
+      await ctrl.toastmsg(res['message'], 'long');
+    }
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -20,47 +61,71 @@ class ForgotPassword extends StatelessWidget {
         backgroundColor: konLightColor2,
       ),
       backgroundColor: konLightColor2,
-      body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 15),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SvgPicture.asset(forgot_password),
-                HeaderTextWidget(label: FORGOT_PASSWORD),
-                HintWidget(label: FORGOT_PASSWORD_HINT),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                  child: TextFormField(
-                    style: mediumTextStyle().copyWith(color: konDarkColorB1),
-                    decoration: textFormFieldInputDecoration('email'),
-                  ),
-                ),
-                ButtonWidget(
-                  value: SEND_LINK,
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Back to ',
-                        style: smallTextStyle().copyWith(color: konDarkColorB1),
+      body: LoadingOverlay(
+        isLoading: _loading,
+        child: SafeArea(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 15),
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(forgot_password),
+                    HeaderTextWidget(label: FORGOT_PASSWORD),
+                    HintWidget(label: FORGOT_PASSWORD_HINT),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                      child: TextFormField(
+                        controller: _email,
+                        validator: (String value) {
+                          if(value.isEmpty){
+                            return 'Email is required';
+                          }
+                          if(!RegExp("^[a-zA-Z0-9.!#%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*").hasMatch(value)){
+                            return 'Enter a valid email address';
+                          }
+                          return null;
+                        },
+                        style: mediumTextStyle().copyWith(color: konDarkColorB1),
+                        decoration: textFormFieldInputDecoration('email'),
                       ),
-                      Text(
-                        SIGN_IN,
-                        style: buttonTextStyle().copyWith(
-                            decoration: TextDecoration.underline,
-                            decorationColor: konPrimaryColor1,
-                            color: konPrimaryColor1),
+                    ),
+                    InkWell(
+                      onTap: _sendlink,
+                      child: ButtonWidget(
+                        value: SEND_LINK,
                       ),
-                    ],
-                  ),
-                )
-              ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Back to ',
+                            style: smallTextStyle().copyWith(color: konDarkColorB1),
+                          ),
+                          InkWell(
+                            onTap: (){
+                              Get.offNamed('/signIn');
+                            },
+                            child: Text(
+                              SIGN_IN,
+                              style: buttonTextStyle().copyWith(
+                                  decoration: TextDecoration.underline,
+                                  decorationColor: konPrimaryColor1,
+                                  color: konPrimaryColor1),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
         ),
